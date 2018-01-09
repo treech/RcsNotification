03d3e8f096aecc30935938202cc75fba9fbfc7e9
yeguoqiang <527922180@qq.com>
2018-01-09 11:08:57 +0800

    仿微信消息通知demo

---
+++ app/src/main/java/com/rcs/notification/Constant.java
@@ -0,0 +1,7 @@
+package com.rcs.notification;
+
+public class Constant {
+    public static final String TARGET_ID = "targetId";
+    public static final String CONVERSATION_TYPE = "conversationType";
+    public static final String TITLE = "title";
+}
